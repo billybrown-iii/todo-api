137ad6d0465cf07462b11e9534d278eb90167a12
Billy <billybdev@gmail.com>
2022-10-29 08:28:41 -0700

    all endpoints added

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-const express = require("express")
-// const validateTodo = require("./functions/validateTodo")
 import Todo from "./models/todo"
+const express = require("express")
+const cors = require("cors")
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // let todos = [
 //   {
@@ -41,14 +43,25 @@ app.post("/api/todos", (req, res, next) => {
 })
 
 app.put("/api/todos/:id", (req, res, next) => {
-  // this only needs to pass in the changed parts, not the whole todo obj
+  // only needs to pass in the changed parts, not the whole todo obj
   Todo.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
     .then(result => {
+      if (!result) return res.status(404).json({ message: "Todo not found" })
       res.status(200).json(result)
     })
     .catch(err => next(err))
 })
 
+app.delete("/api/todos/:id", (req, res, next) => {
+  Todo.findByIdAndDelete(req.params.id)
+    .then(result => {
+      if (!result) return res.status(404).json({ message: "Todo not found" })
+      res.status(202).json(result)
+    })
+    .catch(err => next(err))
+})
+
+// delete all
 app.delete("/api/todos", (req, res, next) => {
   Todo.deleteMany({})
     .then(result => {
@@ -57,10 +70,24 @@ app.delete("/api/todos", (req, res, next) => {
     .catch(err => next(err))
 })
 
+app.get("/info", (req, res, next) => {
+  Todo.countDocuments({})
+    .then(result => {
+      const count = result
+      res.send(`
+        <div>Phone book has info for ${count} todos.</div>
+    <div>Request made at ${new Date()}</div>
+      `)
+    })
+    .catch(err => next(err))
+})
+
 const errorHandler = (err, req, res, next) => {
+  // triggers when the mongoose validations fail
   if (err.name === "ValidatorError" || err.name === "ValidationError")
     return res.status(400).json({ message: err.message })
 
+  // CastError occurs when searching for a malformed Id (an Id of an incorrect format/length)
   if (err.name === "CastError") return res.status(400).json({ message: "Malformed Id" })
 
   // shouldn't reach this part
